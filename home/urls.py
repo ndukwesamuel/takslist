0a410502fd4a6cf24d236371a1e8afab615a02e2
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-08-03 11:12:51 +0100

    new

---
+++ home/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from home.views import apiOverview, taskList, taskDetail, taskCreate, taskUpdate
+
+urlpatterns = [
+    path('', apiOverview, name="apiOverview"),
+    path('task-list/', taskList, name="taskList"),
+    path('task-create/', taskCreate, name="taskCreate"),
+
+
+    path('task-detail/<str:pk>/', taskDetail, name="taskDetail"), 
+
+    path('task-update/<str:pk>/', taskUpdate, name="taskUpdate")
+
+
+
+]
---
+++ home/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+from .models import Task
+from .serializers import TaskSerializer
+
+# Create your views here.
+
+
+def home(request):
+
+    return render(request, "index.html")
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {'List':'/task-list/',}
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def taskDetail(request, pk ):
+    tasks = Task.objects.get(id = pk)
+    serializer = TaskSerializer(tasks, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializerinfo = TaskSerializer(data= request.data)
+
+    if serializerinfo.is_valid():
+        serializerinfo.save()
+
+    return Response(serializerinfo.data)
+
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task,data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
